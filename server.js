940f9cd727522095fd208c38d79c4875d1c87a5e
Breno_vilel4 <teninho1508@gmail.com>
2026-03-31 15:07:37 -0300

    teste server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-await import('./backend/src/server.js');
+import('./backend/src/server.js')
+  .then(() => {
+    console.log('[OK] Backend iniciado via server.js raiz');
+  })
+  .catch((err) => {
+    console.error('[FATAL] Erro ao iniciar backend:', err);
+    process.exit(1);
+  });
